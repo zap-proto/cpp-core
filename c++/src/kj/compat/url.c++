maxy218/capnproto

Use new URL component encoding functions in kj::Url
This has the effect of making the authority, path, and fragment components each slightly more permissive in what they don't escape.

c++/src/kj/compat/url.c++
@@ -375,10 +375,10 @@ String Url::toString(Context context) const {
 
     if (context == REMOTE_HREF) {
       KJ_IF_MAYBE(user, userInfo) {
-        chars.addAll(encodeUriComponent(user->username));
+        chars.addAll(encodeUrlUserInfo(user->username));
         KJ_IF_MAYBE(pass, user->password) {
           chars.add(':');
-          chars.addAll(encodeUriComponent(*pass));
+          chars.addAll(encodeUrlUserInfo(*pass));
         }
         chars.add('@');
       }
@@ -407,7 +407,7 @@ String Url::toString(Context context) const {
       continue;
     }
     chars.add('/');
-    chars.addAll(encodeUriComponent(pathPart));
+    chars.addAll(encodeUrlPath(pathPart));
   }
   if (hasTrailingSlash || (path.size() == 0 && context == HTTP_REQUEST)) {
     chars.add('/');
@@ -427,7 +427,7 @@ String Url::toString(Context context) const {
   if (context == REMOTE_HREF) {
     KJ_IF_MAYBE(f, fragment) {
       chars.add('#');
-      chars.addAll(encodeUriComponent(*f));
+      chars.addAll(encodeUrlFragment(*f));
     }
   }
 

zap-proto/cpp-core

c++/src/kj/compat/url.c++
@@ -375,10 +375,10 @@ String Url::toString(Context context) const {
 
     if (context == REMOTE_HREF) {
       KJ_IF_MAYBE(user, userInfo) {
-        chars.addAll(encodeUriComponent(user->username));
+        chars.addAll(encodeUriUserInfo(user->username));
         KJ_IF_MAYBE(pass, user->password) {
           chars.add(':');
-          chars.addAll(encodeUriComponent(*pass));
+          chars.addAll(encodeUriUserInfo(*pass));
         }
         chars.add('@');
       }
@@ -407,7 +407,7 @@ String Url::toString(Context context) const {
       continue;
     }
     chars.add('/');
-    chars.addAll(encodeUriComponent(pathPart));
+    chars.addAll(encodeUriPath(pathPart));
   }
   if (hasTrailingSlash || (path.size() == 0 && context == HTTP_REQUEST)) {
     chars.add('/');
@@ -427,7 +427,7 @@ String Url::toString(Context context) const {
   if (context == REMOTE_HREF) {
     KJ_IF_MAYBE(f, fragment) {
       chars.add('#');
-      chars.addAll(encodeUriComponent(*f));
+      chars.addAll(encodeUriFragment(*f));
     }
   }
 