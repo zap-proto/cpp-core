MatthewS8/capnproto

[build][perf][capnpc] Allow moving function definitions in generated capnp files out of headers
With CAPNPC_GENERATE_STUBBED_INCLUDES non-templated functions will be declared
as non-inline and moved to the generated source file instead of the header. This
results in massive improvements in include size and compile times for files that
include large capnp headers.
Make this opt-in as the affected functions will no longer be inline, which may
affect performance of code relying on them.

c++/src/capnp/configure.bzl
@@ -4,6 +4,13 @@ def capnp_configure():
     """Generates set of flag, settings for capnp configuration.
     """
 
+    # Define some methods for generated capnp code in source file instead of header, reducing
+    # header parsing overhead but reducing inlining opportunities. Recommended for debug builds.
+    bool_flag(
+        name = "capnpc_generate_stubbed_includes",
+        build_setting_default = False,
+    )
+
     # Generate rust capnp libraries
     bool_flag(
         name = "gen_rust",
@@ -11,6 +18,11 @@ def capnp_configure():
     )
 
     # Settings to use in select() expressions
+    native.config_setting(
+        name = "capnpc_generate_stubbed_includes_true",
+        flag_values = {"capnpc_generate_stubbed_includes": "True"},
+        visibility = ["//visibility:public"],
+    )
     native.config_setting(
         name = "gen_rust_true",
         flag_values = {"gen_rust": "True"},

zap-proto/cpp-core

c++/src/capnp/configure.bzl
@@ -4,6 +4,13 @@ def capnp_configure():
     """Generates set of flag, settings for capnp configuration.
     """
 
+    # Define some methods for generated capnp code in source file instead of header, reducing
+    # header parsing overhead but reducing inlining opportunities. Recommended for debug builds.
+    bool_flag(
+        name = "capnp_no_inline_accessors",
+        build_setting_default = False,
+    )
+
     # Generate rust capnp libraries
     bool_flag(
         name = "gen_rust",
@@ -11,6 +18,11 @@ def capnp_configure():
     )
 
     # Settings to use in select() expressions
+    native.config_setting(
+        name = "capnp_no_inline_accessors_true",
+        flag_values = {"capnp_no_inline_accessors": "True"},
+        visibility = ["//visibility:public"],
+    )
     native.config_setting(
         name = "gen_rust_true",
         flag_values = {"gen_rust": "True"},